36eda2b57ccb4b370839d7c883c6b29c4d50a38b
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-09-17 17:07:01 -0400

    Create CreateBootDisk.bat

---
+++ DISKPART/CreateBootDisk.bat
@@ -0,0 +1,20 @@
+@echo off
+echo.
+
+diskpart /s listdisk.txt
+echo.
+
+set /P diskNum=Enter the disk number (DO NOT USE DISK 0, this is your main hard drive!!): 
+
+echo select %diskNum% > dpScript.txt
+echo clean >> dpScript.txt
+echo create partition primary >> dpScript.txt
+echo select part 1 >> dpScript.txt
+echo format fs=ntfs quick >> dpScript.txt
+echo active >> dpScript.txt
+echo assign >> dpScript.txt
+
+diskpart /s dpScript.txt
+echo.
+
+pause
