92e464731f3e1f1be27a38186519be98c185e36b
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-11-09 10:12:20 -0500

    Update and rename CreateBootDisk.bat to CreateWindowsBootDisk.bat

--- DISKPART/CreateWindowsBootDisk.bat
+++ DISKPART/CreateWindowsBootDisk.bat
@@ -1,11 +1,8 @@
 @echo off
 echo.
-
 diskpart /s listdisk.txt
 echo.
-
 set /P diskNum=Enter the disk number (DO NOT USE DISK 0, this is your main hard drive!!): 
-
 echo select %diskNum% > dpScript.txt
 echo clean >> dpScript.txt
 echo create partition primary >> dpScript.txt
@@ -13,8 +10,6 @@ echo select part 1 >> dpScript.txt
 echo format fs=ntfs quick >> dpScript.txt
 echo active >> dpScript.txt
 echo assign >> dpScript.txt
-
 diskpart /s dpScript.txt
 echo.
-
 pause
