8c7a2bb28147145515ddb00accd2117c22c54f70
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-11-09 12:33:34 -0500

    Update CreateWindowsBootDisk.bat

--- DISKPART/CreateWindowsBootDisk.bat
+++ DISKPART/CreateWindowsBootDisk.bat
@@ -1,11 +1,5 @@
 @echo off
 echo.
-echo Download and open the Windows 11 iso file.  Enter the path
-echo.
-pause.
-echo.
-set /P isofilename=Enter the path to the iso file: 
-echo.
 %Swindir%\system32\diskpart /s listdisk.txt
 echo.
 set /P diskNum=Enter the disk number (DO NOT USE DISK 0, this is your main hard drive!!): 
@@ -16,10 +10,9 @@ echo select part 1 >> dpScript.txt
 echo format fs=ntfs quick >> dpScript.txt
 echo active >> dpScript.txt
 echo assign >> dpScript.txt
+echo.
 %Swindir%\system32\diskpart /s dpScript.txt
 echo.
 echo Bootable media created
 echo.
-echo Mounting and copying iso file to media
-echo.
-%isofilename%
+pause
